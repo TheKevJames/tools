4fdeea5ab301d211b77ed189714c10848228cc94
Kevin James <KevinJames@thekev.in>
2018-05-08 18:01:46 -0700

    feat(slack-notifier): support optional diff url and user tags

--- slack-notifier/send-deploy-notif.sh
+++ slack-notifier/send-deploy-notif.sh
@@ -5,7 +5,7 @@ set -euo pipefail
 DIFF_URL="${CIRCLE_COMPARE_URL:-}"
 NAME="${CIRCLE_PROJECT_REPONAME:-}"
 PREVIOUS="unknown"
-USER="${CIRCLE_USERNAME:-unknown}"
+USER="${CIRCLE_USERNAME:-}"
 VERSION="${CIRCLE_SHA1:-}"
 
 # grab from CLI
@@ -21,10 +21,8 @@ while getopts 'd:n:p:u:v:' flag; do
 done
 
 # verify set
-[[ -z "${DIFF_URL}" ]] && { echo "DIFF_URL is unset"; exit 1; }
 [[ -z "${NAME}" ]] && { echo "NAME is unset"; exit 1; }
 [[ -z "${PREVIOUS}" ]] && { echo "PREVIOUS is unset"; exit 1; }
-[[ -z "${USER}" ]] && { echo "USER is unset"; exit 1; }
 [[ -z "${VERSION}" ]] && { echo "VERSION is unset"; exit 1; }
 
 # build slack payload
@@ -35,9 +33,18 @@ PAYLOAD=$(cat <<EOF
         {
             "fields": [
                 {"title": "Old Version", "value": "${PREVIOUS}", "short": true},
-                {"title": "New Version", "value": "${VERSION}", "short": true},
-                {"title": "Diff", "value": "<${DIFF_URL}|GitHub Diff URL>", "short": true},
-                {"title": "User", "value": "${USER}", "short": true}
+                {"title": "New Version", "value": "${VERSION}", "short": true}
+EOF
+)
+[[ ! -z "${DIFF_URL}" ]] && PAYLOAD+=$(cat <<EOF
+                ,{"title": "Diff", "value": "<${DIFF_URL}|GitHub Diff URL>", "short": true}
+EOF
+)
+[[ ! -z "${USER}" ]] && PAYLOAD+=$(cat <<EOF
+                ,{"title": "User", "value": "${USER}", "short": true}
+EOF
+)
+PAYLOAD+=$(cat <<EOF
             ]
         }
     ]
