5d2bed1835f56ec9019e6183784facf98ca57ed8
Kevin James <kevin@talkiq.com>
2019-02-05 19:09:54 -0800

    feat(slack-notifier): clean up attachments, add changelog option

--- slack-notifier/send-deploy-notif.sh
+++ slack-notifier/send-deploy-notif.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
+CHANGES=""
 # grab from CI
 DIFF_URL="${CIRCLE_COMPARE_URL:-}"
 ENVIRONMENT="${CIRCLE_TAG:-}"
@@ -10,8 +11,9 @@ USER="${CIRCLE_USERNAME:-}"
 VERSION="${CIRCLE_SHA1:-}"
 
 # grab from CLI
-while getopts 'd:e:n:p:u:v:' flag; do
+while getopts 'c:d:e:n:p:u:v:' flag; do
   case "${flag}" in
+    c) CHANGES="${OPTARG}"               ;;
     d) DIFF_URL="${OPTARG}"              ;;
     e) ENVIRONMENT="${OPTARG}"           ;;
     n) NAME="${OPTARG}"                  ;;
@@ -26,27 +28,38 @@ done
 [[ -z "${NAME}" ]] && { echo "NAME is unset"; exit 1; }
 [[ -z "${PREVIOUS}" ]] && { echo "PREVIOUS is unset"; exit 1; }
 [[ -z "${VERSION}" ]] && { echo "VERSION is unset"; exit 1; }
+[[ -z "${SLACK_DEPLOYBOT_WEBHOOK}" ]] && { echo "SLACK_DEPLOYBOT_WEBHOOK is unset"; exit 1; }
 
 # build slack payload
 PAYLOAD=$(cat <<EOF
 {
-    "text": "*ACHTUNG* deploying \`${NAME}\`",
-    "attachments": [
-        {
-            "fields": [
-                {"title": "Version", "value": "${PREVIOUS} -> ${VERSION}", "short": true}
-EOF
-)
-[[ ! -z "${DIFF_URL}" ]] && PAYLOAD=$(cat <<EOF
-                ${PAYLOAD},{"title": "Diff", "value": "<${DIFF_URL}|GitHub Diff URL>", "short": true}
+    "text": "*ACHTUNG* deploying \`${NAME}\`
 EOF
 )
 [[ ! -z "${ENVIRONMENT}" ]] && PAYLOAD=$(cat <<EOF
-                ${PAYLOAD},{"title": "Environment", "value": "${ENVIRONMENT}", "short": true}
+    ${PAYLOAD} (env: \`${ENVIRONMENT}\`)
+EOF
+)
+PAYLOAD=$(cat <<EOF
+    ${PAYLOAD}",
+    "attachments": [
+        {
+            "fields": [
+EOF
+)
+[[ ! -z "${DIFF_URL}" ]] && PAYLOAD=$(cat <<EOF
+                ${PAYLOAD}{"title": "Version", "value": "<${DIFF_URL}|${PREVIOUS} → ${VERSION}>", "short": true}
+EOF
+) || PAYLOAD=$(cat <<EOF
+                ${PAYLOAD}{"title": "Version", "value": "${PREVIOUS} → ${VERSION}", "short": true}
 EOF
 )
 [[ ! -z "${USER}" ]] && PAYLOAD=$(cat <<EOF
                 ${PAYLOAD},{"title": "User", "value": "${USER}", "short": true}
+EOF
+)
+[[ ! -z "${CHANGES}" ]] && PAYLOAD=$(cat <<EOF
+                ${PAYLOAD},{"title": "Changelog", "value": "${CHANGES}"}
 EOF
 )
 PAYLOAD=$(cat <<EOF
