efecd4bf6fe01b0fc16b1c0e0deac5ffcd526b78
Kevin James <KevinJames@thekev.in>
2020-11-25 15:09:02 -0800

    docs(libspacerep): add some TODOs

---
+++ libspacerep/src/spacerep/sm15.nim
@@ -0,0 +1,2 @@
+# TODO: https://gist.github.com/yangchenyun/4942386a91b203bea117
+# TODO: https://github.com/slaypni/SM-15
--- libspacerep/src/spacerep/sm2.nim
+++ libspacerep/src/spacerep/sm2.nim
@@ -1,3 +1,4 @@
+# TODO: https://github.com/walterscarborough/LibSpacey
 import math
 import sequtils
 
