ff6bfca34383394f87f4d25120c42dd7fb27cf86
Kevin James <KevinJames@thekev.in>
2021-01-31 14:19:41 -0800

    feat(poe): add market data to maps page

--- poe/lib/poe_web/controllers/maps_controller.ex
+++ poe/lib/poe_web/controllers/maps_controller.ex
@@ -1,14 +1,15 @@
 defmodule PoeWeb.MapsController do
   require Logger
   use PoeWeb, :controller
+  alias Poe.Api.Ninja
   alias Poe.Maps
 
   def cols() do
     [
-      {:base, "Base Value"},
-      {:alch, "+Alch"},
-      {:chis, "+Chisel"},
-      {:vaal, "+Vaal"},
+      {:base, "Base"},
+      {:alch, "Alch"},
+      {:chis, "Chisel"},
+      {:vaal, "Vaal"},
       {:frag, "Fragment"},
       {:temp, "Prophecy (Tempest)"},
       {:empr, "Prophecy (Extra Monsters)"},
@@ -25,6 +26,21 @@ defmodule PoeWeb.MapsController do
       # {:zper, "Zana (Perandus)"},
       # {:zwar, "Zana (Warbands)"},
       # {:zffb, "Zana (Random)"},
+    ]
+  end
+
+  def prices() do
+    [
+      {:alch, Float.round(Ninja.currency(:alch), 3)},
+      {:chis, Float.round(Ninja.currency(:chis), 3)},
+      {:vaal, Float.round(Ninja.currency(:vaal), 3)},
+      {:frag, Float.round(Ninja.fragment(:sacr), 3)},
+      {:temp, Float.round(Ninja.prophecy(:temp), 3)},
+      {:trap, Float.round(Ninja.prophecy(:trap), 3)},
+      {:empr, Float.round(Ninja.prophecy(:extr), 3)},
+      {:sexs, Float.round(Ninja.currency(:sexs), 3)},
+      {:sexp, Float.round(Ninja.currency(:sexp), 3)},
+      {:sexa, Float.round(Ninja.currency(:sexa), 3)},
     ]
   end
 
@@ -47,6 +63,6 @@ defmodule PoeWeb.MapsController do
   end
 
   def index(conn, _params) do
-    render(conn, "index.html", cols: cols(), rows: rows())
+    render(conn, "index.html", cols: cols(), prices: prices(), rows: rows())
   end
 end
