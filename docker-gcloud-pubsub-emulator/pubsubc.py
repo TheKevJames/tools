ae10200d1c155a80e4a85fab556a5e50b3df35c9
Kevin James <KevinJames@thekev.in>
2023-02-23 18:27:59 +0000

    fix(gcloud-pubsub-emulator): fix support for v407+
    BREAKING CHANGES:
    The image is now based on debian instead of alpine.
    
    Features:
    * support passing arguments to the emulator via docker CMD
    * upgrade to openjdk-11 (from openjdk-8)
    * add readiness debug logs
    * mark port 8682 as EXPOSEd
    
    Fixes:
    * avoid marking container as ready when pubsubc fails
    
    Internal Improvements:
    * rewrite pubsubc as my own python3 lib
    * improve docker build caching order
    * fully reproducible build: pins for wait-for, netcat, python, poetry, etc

---
+++ docker-gcloud-pubsub-emulator/pubsubc.py
@@ -0,0 +1,77 @@
+#!/usr/bin/env python3
+"""
+pubsubc.
+
+Usage:
+
+    export PUBSUB_PROJECT1=project,topic1,topic2:subscription1,...
+    export PUBSUB_PROJECT2=...
+    ./pubsubc.py
+
+At least $PUBSUB_PROJECT1 must be set, and it must have at least the project
+and one topic listed. Topics may have any number of subscription, each
+separated by a colon.
+
+To run against an emulator, use:
+
+    export PUBSUB_EMULATOR_HOST=localhost:8681
+
+Credit:
+
+    This script was originally ported from: https://github.com/prep/pubsubc
+"""
+import os
+import sys
+
+from google.cloud import pubsub_v1  # type: ignore[attr-defined]
+
+
+def create(project: str, topics: list[str]) -> None:
+    publisher = pubsub_v1.PublisherClient()
+    subscriber = pubsub_v1.SubscriberClient()
+    print(f'configuring project: {project}')
+
+    for spec in topics:
+        topic, *subscriptions = spec.split(':')
+
+        print(f'- creating topic: {topic}')
+        topic_name = publisher.topic_path(project, topic)
+        publisher.create_topic(name=topic_name)
+
+        for subscription in subscriptions:
+            print(f'  - creating subscription: {subscription}')
+            subscription_name = subscriber.subscription_path(
+                project,
+                subscription,
+            )
+            subscriber.create_subscription(
+                name=subscription_name,
+                topic=topic_name,
+            )
+
+
+def main() -> None:
+    i = 0
+    while True:
+        i += 1
+        env = os.environ.get(f'PUBSUB_PROJECT{i}')
+        if not env:
+            if i == 1:
+                print(__doc__)
+                print()
+                print('ERROR: $PUBSUB_PROJECT1 was not set')
+                sys.exit(1)
+            return
+
+        project, *topics = env.split(',')
+        if not topics:
+            print(__doc__)
+            print()
+            print(f'ERROR: $PUBSUB_PROJECT{i} had no defined topic')
+            sys.exit(1)
+
+        create(project, topics)
+
+
+if __name__ == '__main__':
+    main()
--- docker-gcloud-pubsub-emulator/run.sh
+++ docker-gcloud-pubsub-emulator/run.sh
@@ -1,11 +1,16 @@
 #!/bin/sh
-
 # Start the PubSub client in the background. It will poll for an open PubSub
 # emulator port and create its topics and subscriptions when it's up.
 #
 # After it's done, port 8682 will be open to facilitate the wait-for and
 # wait-for-it scripts.
-(/usr/bin/wait-for localhost:8681 -- env PUBSUB_EMULATOR_HOST=localhost:8681 /usr/bin/pubsubc -debug; nc -lkp 8682 >/dev/null) &
+(
+    set -eu
+    echo "Waiting for emulator..."
+    /usr/bin/wait-for localhost:8681 -- env PUBSUB_EMULATOR_HOST=localhost:8681 /usr/bin/pubsubc
+    echo "Done building projects/topics/subscriptions! Opening readiness port..."
+    nc -lkp 8682
+) &
 
 # Start the PubSub emulator in the foreground.
 gcloud beta emulators pubsub start --host-port=0.0.0.0:8681 "$@"
