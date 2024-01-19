a00b69db243a549d0bd0550ef7ac887a8d88072f
Kevin James <KevinJames@thekev.in>
2024-01-19 21:59:06 +0000

    feat(gcloud-pubsub-emulator): support push subscriptions and file configs (#727)
    * feat(gcloud-pubsub-emulator): prettyprint compiled config
    * feat(gcloud-pubsub-emulator): support json file config
    * refactor(gcloud-pubsub-emulator): use dataclasses for configs
    * feat(gcloud-pubsub-emulator): support push subscriptions

--- docker-gcloud-pubsub-emulator/pubsubc.py
+++ docker-gcloud-pubsub-emulator/pubsubc.py
@@ -4,12 +4,18 @@ pubsubc.
 
 Usage:
 
+    export PUBSUBC_CONFIG=/my/config.json  # default: /etc/pubsubc/config.json
+    ./pubsubc.py
+
+If a configuration file is not found, you may alternatively configure pubsubc
+via environment variables:
+
     export PUBSUB_PROJECT1=project,topic1,topic2:subscription1,...
     export PUBSUB_PROJECT2=...
     ./pubsubc.py
 
 At least $PUBSUB_PROJECT1 must be set, and it must have at least the project
-and one topic listed. Topics may have any number of subscription, each
+and one topic listed. Topics may have any number of subscriptions, each
 separated by a colon.
 
 To run against an emulator, use:
@@ -18,59 +24,136 @@ To run against an emulator, use:
 
 Credit:
 
-    This script was originally ported from: https://github.com/prep/pubsubc
+    This script was based on: https://github.com/prep/pubsubc
 """
+import dataclasses
+import json
 import os
 import sys
+from collections.abc import Iterator
+from typing import Optional
 
 from google.cloud import pubsub_v1  # type: ignore[attr-defined]
 
 
-def create(project: str, topics: list[str]) -> None:
-    publisher = pubsub_v1.PublisherClient()
-    subscriber = pubsub_v1.SubscriberClient()
-    print(f'configuring project: {project}')
-
-    for spec in topics:
-        topic, *subscriptions = spec.split(':')
-
-        print(f'- creating topic: {topic}')
-        topic_name = publisher.topic_path(project, topic)
-        publisher.create_topic(name=topic_name)
-
-        for subscription in subscriptions:
-            print(f'  - creating subscription: {subscription}')
-            subscription_name = subscriber.subscription_path(
-                project,
-                subscription,
-            )
-            subscriber.create_subscription(
-                name=subscription_name,
-                topic=topic_name,
-            )
+@dataclasses.dataclass
+class SubscriptionConfig:
+    name: str
+    endpoint: Optional[str] = None
 
 
-def main() -> None:
+@dataclasses.dataclass
+class TopicConfig:
+    name: str
+    project: str
+    subscriptions: list[SubscriptionConfig] = dataclasses.field(
+        default_factory=list,
+    )
+
+
+def pprint(x: str, levels: int = 0) -> None:
+    indent = ' ' * ((levels * 2) + 1)
+    print(f'[pubsubc]{indent}{x}')
+
+
+def build_config() -> Iterator[TopicConfig]:
+    fname = os.environ.get('PUBSUBC_CONFIG', '/etc/pubsubc/config.json')
+    try:
+        with open(fname, encoding='utf-8') as f:
+            config = json.load(f)
+
+        pprint(f'loading config from file ({fname})...')
+        for spec in config:
+            subscriptions = [
+                SubscriptionConfig(sub['name'], sub.get('push_endpoint'))
+                for sub in spec.get('subscriptions') or []
+            ]
+            yield TopicConfig(spec['topic'], spec['project'], subscriptions)
+
+        if os.environ.get('PUBSUB_PROJECT1'):
+            pprint('WARN: $PUBSUB_PROJECT1 is set but loaded config from file')
+            pprint('      environment variables should be unset')
+
+        return
+    except FileNotFoundError:
+        pprint('loading config from env...')
+    except Exception:
+        pprint(__doc__)
+        print()
+        pprint('ERROR: invalid configuration file')
+        sys.exit(1)
+
     i = 0
     while True:
         i += 1
         env = os.environ.get(f'PUBSUB_PROJECT{i}')
         if not env:
-            if i == 1:
-                print(__doc__)
-                print()
-                print('ERROR: $PUBSUB_PROJECT1 was not set')
-                sys.exit(1)
             return
 
         project, *topics = env.split(',')
         if not topics:
-            print(__doc__)
-            print()
-            print(f'ERROR: $PUBSUB_PROJECT{i} had no defined topic')
-            sys.exit(1)
+            return
 
-        create(project, topics)
+        for spec in topics:
+            topic_name, *subscription_names = spec.split(':')
+            subscriptions = [
+                SubscriptionConfig(name)
+                for name in subscription_names or []
+            ]
+            yield TopicConfig(topic_name, project, subscriptions)
+
+
+def print_config(config: list[TopicConfig]) -> None:
+    pprint('built pubsubc config:')
+    for topic in config:
+        pprint(f'topic: {topic.name} (project: {topic.project})', 1)
+        for subscription in topic.subscriptions:
+            pprint(f'subscription: {subscription.name}', 2)
+            if subscription.endpoint:
+                pprint(f'push endpoint: {subscription.endpoint}', 3)
+
+
+def create(topic: TopicConfig) -> None:
+    publisher = pubsub_v1.PublisherClient()
+    subscriber = pubsub_v1.SubscriberClient()
+    pprint(f'configuring topic: {topic.name} in project: {topic.project}')
+
+    pprint(f'- creating topic: {topic.name}')
+    topic_name = publisher.topic_path(topic.project, topic.name)
+    publisher.create_topic(name=topic_name)
+
+    for subscription in topic.subscriptions:
+        pprint(f'  - creating subscription: {subscription.name}')
+
+        c: Optional[pubsub_v1.types.PushConfig]
+        if subscription.endpoint:
+            pprint(f'    using push endpoint: {subscription.endpoint}')
+            c = pubsub_v1.types.PushConfig(push_endpoint=subscription.endpoint)
+        else:
+            c = None
+
+        subscription_name = subscriber.subscription_path(
+            topic.project,
+            subscription.name,
+        )
+        subscriber.create_subscription(
+            name=subscription_name,
+            topic=topic_name,
+            push_config=c,
+        )
+
+
+def main() -> None:
+    config = list(build_config())
+    if not config:
+        pprint(__doc__)
+        print()
+        pprint('ERROR: at least one topic must be provided')
+        sys.exit(1)
+
+    print_config(config)
+    for topic in config:
+        create(topic)
 
 
 if __name__ == '__main__':
